a5546ea338af871da08ceb524011b05b97c58d91
Marcos <marcos.kukuchka@gmail.com>
2022-10-27 21:18:23 -0300

    test uptadate

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,20 +1,13 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'postgres'),
-        ssl: { rejectUnauthorized: false }
-      },
-      options: {
-        ssl: false
-      },
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'postgres'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f64b3d1e3ada275e23266433cb898a50'),
-    },
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
